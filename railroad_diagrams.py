f662c7e52b0a5805054f57b8df9811a7f30705e4
Tab Atkins <jackalmage@gmail.com>
2013-10-28 12:48:34 -0700

    Push the default stylesheet into the module-as-script output.

--- railroad_diagrams.py
+++ railroad_diagrams.py
@@ -394,5 +394,5 @@ if __name__ == '__main__':
         sys.stdout.write('\n')
 
     import sys
-    sys.stdout.write("<!doctype html><title>Test</title><link href='railroad-diagrams.css' rel=stylesheet>")
+    sys.stdout.write("<!doctype html><title>Test</title><style>svg.railroad-diagram{background-color:hsl(30,20%,95%);}svg.railroad-diagram path{stroke-width:3;stroke:black;fill:rgba(0,0,0,0);}svg.railroad-diagram text{font:bold 14px monospace;text-anchor:middle;}svg.railroad-diagram text.label{text-anchor:start;}svg.railroad-diagram text.comment{font:italic 12px monospace;}svg.railroad-diagram rect{stroke-width:3;stroke:black;fill:hsl(120,100%,90%);}</style>")
     exec(open('css-example.py-js').read())
