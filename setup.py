dd9597ed795923f45aa4b5b5a20d376d362b6b11
Lucas Wiman <lucas.wiman@gmail.com>
2017-02-10 22:51:06 -0800

    Add packaging metadata.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+    name='railroad-diagrams',
+    py_modules=['railroad_diagrams'],
+    version='0.0.1',
+    description='Generate SVG railroad syntax diagrams.',
+    author='Tab Atkins',
+    author_email='jackalmage@gmail.com',
+    url='https://github.com/tabatkins/railroad-diagrams',
+    keywords=['diagrams', 'syntax', 'grammar', 'railroad diagrams'],
+    classifiers=[
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
